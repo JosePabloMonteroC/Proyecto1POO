53ee8d4e0bae56d7dedb806e3184a7147ae346a3
Roman <Roman@iMac>
2022-03-09 19:34:12 -0600

    ultimo commit

--- Proyecto1POO/src/application/Main.java
+++ Proyecto1POO/src/application/Main.java
@@ -21,13 +21,20 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		
+		
 		DataWorker obj1 = new DataWorker();
         String[] palabras = {"Max","Hamilton","FIA"};
-        obj1.setListPalabras(obj1.regresaArrayList("C:\\Users\\pepem\\git\\Proyecto1POO\\Proyecto1POO\\src\\application\\temporal1.csv"));
+        obj1.setListPalabras(obj1.regresaArrayList("C:\\Users\\Roman\\Downloads\\archive\\temporal1.csv"));
         obj1.setListPalabras(obj1.eliminarCaracteres(obj1.getListPalabras()));
         obj1.setPalabras(palabras);
         obj1.setTweets(obj1.contarPalabras(obj1.getPalabras(),obj1.getListPalabras()));
         obj1.imprimirPalabras(obj1.getTweets(), obj1.getPalabras());
-		launch(args);
+        
+        
+        launch(args);
+        
+        
+        
 	}
 }
